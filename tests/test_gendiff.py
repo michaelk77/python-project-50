95b89db2542a2667869bb04323de9b122ac1f403
michael <catcat2005@inbox.ru>
2023-03-13 13:17:56 +0100

    tests pep8

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -3,8 +3,8 @@ from gendiff.generate_diff import generate_diff
 
 test_data = [("tests/fixtures/file1.json", "tests/fixtures/file2.json",
               "tests/fixtures/file3.txt", "stylish"), (
-                 "tests/fixtures/file4.yaml", "tests/fixtures/file5.yaml",
-                 "tests/fixtures/file6.txt", "stylish"),
+             "tests/fixtures/file4.yaml", "tests/fixtures/file5.yaml",
+             "tests/fixtures/file6.txt", "stylish"),
              ("tests/fixtures/file1.json", "tests/fixtures/file2.json",
               "tests/fixtures/file7.txt", "plain"),
              ("tests/fixtures/file1.json", "tests/fixtures/file2.json",
